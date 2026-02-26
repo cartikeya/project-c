53f929d55abc6c26b8b44f012d7bd44f8ec99cb1
cartikeya <cartikeya.official@gmail.com>
2026-02-26 11:59:44 +0530

    changed backend url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,12 +1,12 @@
 import io from "socket.io-client";
-// export const socket = io.connect("https://project-c-jrzu.onrender.com/");
+export const socket = io.connect("https://project-c-jrzu.onrender.com/");
 
 
 
 
 
 // Comment out the live Render URL for now!
-export const socket = io.connect("https://ipl-auction-backend.onrender.com");
+// export const socket = io.connect("https://ipl-auction-backend.onrender.com");
 
 // Use your local server while adding new features:
 // export const socket = io.connect("http://localhost:3001");
